2edd661fdce2303717dffd2fa65cb6d445551ef3
Vikrant Shete <tejuviky2005@gmail.com>
2019-11-20 17:44:51 +0530

    Added test cases for Functional component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { create } from "react-test-renderer";
+import FunctionalComponent from './function/FunctionalComponent';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe("Functional component", () => {
+  test("should have only square class if nothing is passed in props", () => {
+    const comp = create(<FunctionalComponent/>);
+    expect(comp.toJSON().props.className).toBe('square undefined');
+  });
+
+  test("should have square and any given classes if they are passed in props", () => {
+    const comp = create(<FunctionalComponent className="classOne classTwo"/>);
+    expect(comp.toJSON().props.className).toBe('square classOne classTwo');
+  });
 });
